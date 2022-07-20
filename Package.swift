b607fecaab7ee2decc3bd4d1856803cdfaa0c963
Jaehong Kang <sinoru@me.com>
2022-07-20 20:34:17 +0900

    Downgrade to swift-tools-version 5.3

--- Package.swift
+++ Package.swift
@@ -1,17 +1,10 @@
-// swift-tools-version: 5.5
+// swift-tools-version: 5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "swift-snowflake",
-    platforms: [
-         .macOS(.v12),
-         .macCatalyst(.v15),
-         .iOS(.v15),
-         .tvOS(.v15),
-         .watchOS(.v7),
-    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
